dfdde9ec62e3769872070514159dff147dc1e663
LeeeSe <724818342@qq.com>
2026-09-18 17:57:31 +0800

    build: bundle fixed debug.keystore in repository for cross-machine seamless upgrades

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,15 @@ android {
         }
     }
 
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
